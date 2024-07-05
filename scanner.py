8235a7fd3c7b791519bd44cde606a69c9197bbce
aakbar4008 <91166870+aakbar4008@users.noreply.github.com>
2024-07-05 17:03:57 -0400

    Add files via upload

---
+++ scanner.py
@@ -0,0 +1,9 @@
+import socket 
+for port in range(5000, 5005):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    result = sock.connect_ex(("192.168.0.250", port))
+    if result == 0:
+        print("Port " + str(port) + " is open")
+    else:
+        print("Port " + str(port) +  " is closed")
+    sock.close()
